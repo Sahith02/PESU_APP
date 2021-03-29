6a116c91661feaaaaeb8957500d19839fb6c8499
revanthbabu <putturevanth@gmail.com>
2021-03-29 14:10:20 +0530

    sending dbconn obj from mainapp

--- FeedBack.py
+++ FeedBack.py
@@ -8,20 +8,20 @@ class FeedBack:
 		self.StudentID = student
 		self.Review = None
 	
-	def ViewReview(self):
-		cnx = mysql.connector.connect(host = "localhost", user = "root", database = "mydatabase")
-		cur = cnx.cursor()
+	def ViewReview(self, dbconn):
+		#cnx = mysql.connector.connect(host = "localhost", user = "root", database = "mydatabase")
+		cur = dbconn.cursor()
 		q = "SELECT * FROM FeedBack WHERE CourseID = %s AND StudentID = %s"
 		cur.execute(q, (self.CourseID, self.StudentID))
 		res = cur.fetchone()
 		self.Review = res[2]
 		return self.Review
 	
-	def WriteReview(self, review):
+	def WriteReview(self, review, dbconn):
 		self.Review = review
 		try:
-			cnx = mysql.connector.connect(host = "localhost", user = "root", database = "mydatabase")
-			cur = cnx.cursor()
+			#cnx = mysql.connector.connect(host = "localhost", user = "root", database = "mydatabase")
+			cur = dbconn.cursor()
 			q = "INSERT INTO FeedBack VALUES(%s, %s, %s)"
 			cur.execute(q, (self.CourseID, self.StudentID, review))
 			return True
