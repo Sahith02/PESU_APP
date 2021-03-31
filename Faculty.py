d71f1a0739804f81eb963756a502aea430a8110f
revanthbabu <putturevanth@gmail.com>
2021-03-31 13:47:16 +0530

    modified Faculty to get details from db

--- Faculty.py
+++ Faculty.py
@@ -1,24 +1,33 @@
-#faculty table will be ID,email(1* key),Phone number,Address,DateofJoining
+#Faculty table will be FacultyID, email(1* key), ContactNumber, Address, DateofJoining
 
 class Faculty():
-	def __init__(self,email=""):
-		self.email=email
-		#get below from the database
-		'''
-		self.FacultyID = id
-		self.ContactNumber = phno
-		self.Address = address
-		self.DateOfJoining = doj
-		'''
+	def __init__(self, email, dbconn):
+		self.cur = dbconn.cursor()
+		query = "SELECT * FROM Faculty WHERE email = %s"
+		cur.execute(query, (email,))
+		res = cur.fetchone()
+		self.FacultyID = res[0]
+		self.email = res[1]
+		self.ContactNumber = res[2]
+		self.Address = res[3]
+		self.DateOfJoining = res[4]
 		
-	def EditDetails(self):
-		#self.ContactNumber = phno
-		#self.Address = address
-		pass
+	def EditDetails(self, email = None, ContactNumber = None, Address = None):
+		if ContactNumber:
+			query = "UPDATE Faculty SET ContactNumber = %s WHERE FacultyID = %s"
+			self.cur.execute(query, (ContactNumber,self.FacultyID))
+		if Address:
+			query = "UPDATE Faculty SET Address = %s WHERE FacultyID = %s"
+			self.cur.execute(query, (ContactNumber, self.FacultyID))
+		if email:
+			query = "UPDATE Faculty SET email = %s WHERE FacultyID = %s"
+			self.cur.execute(query, (email , self.FacultyID))
 		
 	def CheckEventNotification(self,notifications):
 		pass
 	
+	def __del__(self):
+		self.cur.close()
+
 	def __repr__(self):
 		return "\nStudent:\nID = {0}\n".format(self.email)
-	
